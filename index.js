cd88de968d5d8c6090b38ff701adff659f130f6a
tolmdyn <tolmdyn@gmail.com>
2024-07-26 19:56:25 +0100

    first commit with sim, webpage and chart

---
+++ index.js
@@ -0,0 +1,121 @@
+// import {Rocket, RocketSimulation} from "./rocketsim"
+
+const simConfig = {
+  timestep: 0.1,
+  gravity: 9.8,
+  maxTimesteps: 1000,
+}
+
+const rocketConfig = {
+  diameter: 0.035306,
+  mass: 0.106,
+  massCase: 0.01442,
+  impulse: 9,
+  thrust: 6,
+  dragCoefficient: 0.75,
+  ejectionDelay: 7,
+  chuteDiameter: 0.3,
+  propellantMass: 0.01248,
+}
+
+let rocket = new Rocket(rocketConfig);
+let simulation = new RocketSimulation({ rocket, ...simConfig });
+
+simulation.simulate();
+
+// Log to console:
+// simulation.printResults();
+// simulation.printMaxAltitude();
+
+// ===================== Display ===================
+
+function populateResultsTable() {
+  const tableBody = document.querySelector('#resultsTable tbody');
+  tableBody.innerHTML = ''; // Clear existing data
+
+  simulation.results.forEach(result => {
+    const row = document.createElement('tr');
+    row.innerHTML = `
+      <td>${result.time.toFixed(1)}</td>
+      <td>${result.drag.toFixed(6)}</td>
+      <td>${result.netForce.toFixed(6)}</td>
+      <td>${result.mass.toFixed(6)}</td>
+      <td>${result.acceleration.toFixed(6)}</td>
+      <td>${result.velocity.toFixed(6)}</td>
+      <td>${result.altitude.toFixed(6)}</td>
+    `;
+    tableBody.appendChild(row);
+  });
+}
+
+function displayMaxAltitude() {
+  const result = simulation.getMaxAltitude();
+  const maxAltitudeElement = document.getElementById('maxAltitude');
+  maxAltitudeElement.textContent = `Max altitude: ${result.max.toFixed(2)} meters at time: ${result.time.toFixed(1)} seconds`;
+}
+
+populateResultsTable();
+displayMaxAltitude();
+
+// ===================== Chart ===================
+
+const commonDatasetConfig = {
+  borderColor: 'rgba(75, 192, 192, 1)', // Default line color
+  backgroundColor: 'rgba(75, 192, 192, 0.2)', // Default fill color
+  borderWidth: 2, // Line thickness
+  pointRadius: 0, // Hide points
+  tension: 0.1 // Smooth line
+};
+
+function createDataset(label, data, color) {
+  return {
+    ...commonDatasetConfig,
+    label: label,
+    data: data,
+    borderColor: color,
+    backgroundColor: color.replace('1)', '0.4)')
+  };
+}
+
+const times = simulation.results.map(result => result.time);
+const altitudes = simulation.results.map(result => result.altitude);
+const velocities = simulation.results.map(result => result.velocity);
+const accelerations = simulation.results.map(result => result.acceleration);
+
+const altitudeDataset = createDataset('Altitude (m)', altitudes, 'rgba(75, 192, 192, 1)');
+const velocityDataset = createDataset('Velocity (m/s)', velocities, 'rgba(255, 99, 132, 1)'); // Different color
+const accelerationDataset = createDataset('Acceleration (m/s²)', accelerations, 'rgba(250, 200, 90, 1)'); // Different color
+
+const ctx = document.getElementById('resultsChart');
+
+new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: times,
+    datasets: [altitudeDataset, velocityDataset, accelerationDataset]
+  },
+  options: {
+    scales: {
+      x: {
+        title: {
+          display: true,
+          text: 'Time (s)'
+        },
+        type: 'linear',
+        ticks: {
+          callback: function (value) {
+            return value.toFixed(0);
+          },
+          stepSize: 1,
+        }
+      },
+      y: {
+        title: {
+          display: true,
+          text: 'Results' // Y-axis title
+        },
+        beginAtZero: true // Start Y-axis at zero
+      }
+    }
+  }
+});
---
+++ rocketsim.js
@@ -0,0 +1,166 @@
+
+class Rocket {
+  constructor(parameters) {
+    this.diameter = parameters.diameter;  // rocket diameter (D)
+    this.mass = parameters.mass;          // rocket mass (Mr)
+    this.massCase = parameters.massCase;  // estes motor mass (0.0107 or 0.01442) (Mc)
+    this.impulse = parameters.impulse;    // (I)
+    this.thrust = parameters.thrust;      // (Ft)
+    this.dragCoefficient = parameters.dragCoefficient; // (Cd)
+    this.ejectionDelay = parameters.ejectionDelay;     // (tc)
+    this.chuteDiameter = parameters.chuteDiameter;     // (ChD)
+    this.propellantMass = parameters.propellantMass   // 
+
+    // Calculated properties
+    this.burnTime = this.impulse / this.thrust;        // Estimate burn time (tb)
+    this.massDecrement = this.propellantMass / this.burnTime; // Compute mass decrement (dM)
+    this.bodyArea = Math.PI * Math.pow(this.diameter, 2) / 4;  // Area of rocket body (A)
+    this.chuteArea = Math.PI * Math.pow(this.chuteDiameter, 2) / 4;  // Area of chute (ChA)
+  }
+
+  calculate_thrust(time) {
+    if (time < this.burnTime) {
+      return this.thrust;
+    } else {
+      return 0;
+    }
+  }
+
+  calculate_drag(time, velocity, altitude) {
+    const airDensity = 1.22 * Math.pow(0.9, altitude / 1000); // Air density decreases with altitude
+    const area = (time < this.burnTime + this.ejectionDelay) ? this.bodyArea : Math.max(this.bodyArea, this.chuteArea);
+    return 0.5 * airDensity * this.dragCoefficient * area * Math.sign(velocity) * Math.pow(velocity, 2);
+  }
+
+  update_mass(time) {
+    // update mass loss due to fuel consumption
+    if (time < this.burnTime) {
+      return this.mass + this.massCase + this.propellantMass - this.massDecrement * time;
+    } else {
+      return this.mass + this.massCase;
+    }
+  }
+}
+
+class RocketSimulation {
+  constructor(parameters) {
+    this.rocket = parameters.rocket;  // The rocket object to sim
+    this.timestep = parameters.timestep;          // Time Step (dt)
+    this.gravity = parameters.gravity;            // Gravity (g)
+    this.maxTimesteps = parameters.maxTimesteps;        // Size of simulation: simulation time is dt * mAS 
+
+    // Array to store simulation frame steps as objects
+    this.results = [];
+  }
+
+  simulate() {
+    let time = 0;
+    let prevAltitude = 0;
+    let prevVelocity = 0;
+
+    const landingThreshold = 5;
+    let consecutiveZeroAltitudeCount = 0;
+
+    for (let idx = 0; idx < this.maxTimesteps; idx++) {
+      time = idx * this.timestep;
+      let mass = this.rocket.update_mass(time);
+      let thrust = this.rocket.calculate_thrust(time);
+      let drag = this.rocket.calculate_drag(time, prevVelocity, prevAltitude);
+      let netForce = thrust - drag - mass * this.gravity;
+      let acceleration = netForce / mass;
+      let velocity = prevVelocity + acceleration * this.timestep;
+      let newAltitude = prevAltitude + velocity * this.timestep + 0.5 * acceleration * Math.pow(this.timestep, 2);
+
+      // If the new altitude is below ground level, set it to 0
+      if (newAltitude < 0) {
+        newAltitude = 0;
+        velocity = 0;
+        acceleration = 0;
+      }
+
+      // Store results
+      this.results.push({
+        time: time,
+        drag: drag,
+        netForce: netForce,
+        mass: mass,
+        acceleration: acceleration,
+        velocity: velocity,
+        altitude: newAltitude,
+      });
+
+      // Check if the altitude has been zero for the last `landingThreshold` iterations
+      if (newAltitude === 0) {
+        consecutiveZeroAltitudeCount++;
+      } else {
+        consecutiveZeroAltitudeCount = 0;
+      }
+
+      if (consecutiveZeroAltitudeCount >= landingThreshold) {
+        break;
+      }
+
+      // Update previous values for next iteration
+      prevAltitude = newAltitude;
+      prevVelocity = velocity;
+
+    }
+  }
+
+  getMaxAltitude() {
+    if (this.results.length === 0) {
+      return -1; // error
+    }
+
+    let imax = 0;
+    let ymax = this.results[0].altitude;
+
+    for (let i = 1; i < this.results.length; i++) {
+      if (this.results[i].altitude > ymax) {
+        imax = i;
+        ymax = this.results[i].altitude;
+      }
+    }
+    // console.log(`Max altitude: ${ymax} At time: ${imax / 10}`);
+    return { max: ymax, time: this.results[imax].time };
+  }
+
+  printMaxAltitude() {
+    const result = simulation.getMaxAltitude();
+    console.log(`Max altitude: ${result.max} at time: ${result.time}`);
+  }
+
+  printResults() {
+    this.results.forEach(result => {
+      console.log(`t: ${(result.time).toFixed(1)}, Fd: ${(result.drag).toFixed(6)}, F: ${(result.netForce).toFixed(6)}, M: ${(result.mass).toFixed(6)}, Acc: ${(result.acceleration).toFixed(6)}, Vel: ${(result.velocity).toFixed(6)}, Alt: ${(result.altitude).toFixed(6)}`);
+    });
+  }
+}
+
+// ========================== Usage ==============================
+
+// const simConfig = {
+//   timestep: 0.1,
+//   gravity: 9.8,
+//   maxTimesteps: 1000,
+// }
+
+// const rocketConfig = {
+//   diameter: 0.035306,
+//   mass: 0.106,
+//   massCase: 0.01442,
+//   impulse: 9,
+//   thrust: 6,
+//   dragCoefficient: 0.75,
+//   ejectionDelay: 7,
+//   chuteDiameter: 0.3,
+//   propellantMass: 0.01248,
+// }
+
+// let rocket = new Rocket(rocketConfig);
+// let simulation = new RocketSimulation({ rocket, ...simConfig });
+
+// simulation.simulate();
+
+// simulation.printResults();
+// simulation.printMaxAltitude();
